09396d358701a20019a1a98330a54a5464368d06
Artur Kedzior <artur@kedzior.io>
2018-09-13 15:49:48 +0200

    - Build and CI config

---
+++ Build.ps1
@@ -0,0 +1,19 @@
+function Exec  
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+
+$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$revision = "beta-{0:D4}" -f [convert]::ToInt32($revision, 10)
+
+exec { & dotnet pack .t -c Release -o .\artifacts --version-suffix=$revision }
